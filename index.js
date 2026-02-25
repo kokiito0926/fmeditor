0fa0e82368be139519264a8d683a20e7184e52da
Koki Ito <kokiito0926@gmail.com>
2026-02-25 17:32:53 +0900

    feat: 🎸 プレイグラウンドからファイルをコピーした

---
+++ index.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+import { stdin, argv } from "zx";
+import matter from "gray-matter";
+
+if (process.stdin.isTTY) {
+	process.exit(1);
+}
+
+const input = await stdin();
+if (!input) {
+	process.exit(1);
+}
+
+const { _, ...flags } = argv;
+
+const processedData = {};
+for (const [key, value] of Object.entries(flags)) {
+	try {
+		processedData[key] = JSON.parse(value);
+	} catch (err) {
+		processedData[key] = value;
+	}
+}
+
+const doc = matter(input || "");
+
+doc.data = { ...doc.data, ...processedData };
+
+const text = matter.stringify(doc.content, doc.data);
+console.log(text);
